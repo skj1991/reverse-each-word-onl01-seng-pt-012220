87aa8f282325417324fa9d26c342d9fe8638b24b
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 16:10:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,8 @@ def reverse_each_word(sentence)
   #reversed = []
   sentence.split.collect do |word|
     #reversed <<
-    word.reverse.join(" ")
+    word.reverse.
   end
+  sentence.join(" ")
   #reversed.join(" ")
 end
